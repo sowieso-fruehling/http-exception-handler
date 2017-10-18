b07ad76969d3f511742a5e5d1782ae29a4200d94
sowieso-fruehling <milosoa@gmail.com>
2017-10-18 22:38:16 +0200

    Update MyExceptionHandler.java

--- src/main/java/de/be/aff/handler/MyExceptionHandler.java
+++ src/main/java/de/be/aff/handler/MyExceptionHandler.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import javax.servlet.http.HttpServletRequest;
+import org.springframework.web.client.HttpStatusCodeException;
 
 @ControllerAdvice //applies to the whole app, not to specific controller. Is not only for exceptions
 //@ControllerAdvice(assignableTypes = MyController.class) //if we want it to apply only to MyController class
